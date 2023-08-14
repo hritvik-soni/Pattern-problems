37fdb2772214c0f92222a52d696c33848a604970
Hritvik Soni <hritviksoni768@gmail.com>
2023-08-14 16:12:43 +0530

    added

---
+++ pattern-7.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class pattern7 {
+    public static void main(String[] args) {
+         Scanner s= new Scanner(System.in);
+         int n = s.nextInt();
+        
+         
+         for(int i=1;i<=n;i++){
+
+            if(i==1 || i== n){
+
+                for(int j=1; j<=n;j++){
+                    System.out.print("* ");
+                }
+
+            }
+            else{
+                System.out.print("* ");
+
+                 for(int j=1; j<n-1;j++){
+                    System.out.print("  ");
+                }
+                System.out.print("*");
+            }
+           
+         System.out.println();
+         }
+    }
+}
