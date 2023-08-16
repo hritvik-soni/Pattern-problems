247b7641dd4b6cd4f70ae51e80a6dbfda6e7029d
Hritvik Soni <hritviksoni768@gmail.com>
2023-08-16 20:24:25 +0530

    Added

---
+++ pattern-27.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class pattern27 {
+    public static void main(String[] args) {
+         Scanner s= new Scanner(System.in);
+         int n = s.nextInt();
+         int star =1;
+         int space=n-1;
+         
+         for(int i=0;i<n;i++){
+
+            for(int j=0;j<space;j++){
+               System.out.print("  ");
+            }
+             int p=1;
+             for(int j=0;j<star;j++){
+               System.out.print(p+" ");
+               if(j<star/2){
+                 p++;
+               }
+               else{
+                  p--;
+               }
+              
+            }
+         space--;
+         star+=2;
+         System.out.println();
+         }
+           s.close();
+    }
+}
